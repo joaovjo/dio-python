b1e231b0d4839b2b5cb8e1e8e6a0b01480b3182b
João Vitor de Jesus Oliveira <contato@joaovjo.com.br>
2024-12-13 15:55:43 -0300

    refactor: SQLAlchemy agora substitui o db.py na inicialização

--- python-backend/flask-api/dio_bank/src/app.py
+++ python-backend/flask-api/dio_bank/src/app.py
@@ -1,4 +1,25 @@
-from flask import Flask
+import os
+import click
+
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+@click.command("init-db")  # Define um comando de linha de comando chamado "init-db"
+def init_db_command():  # Função que será executada quando o comando "init-db" for chamado
+    """Clear the existing data and create new tables."""  # Descrição do comando
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo("Initialized the database.")  # Exibe uma mensagem indicando que o banco de dados foi inicializado
 
 
 def create_app(test_config=None):
@@ -6,7 +27,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        DATABASE="diobank.sqlite",
+        SQLALCHEMY_DATABASE_URI=os.path.join("sqlite:///diobank.sqlite"),
     )
 
     if test_config is None:
@@ -16,13 +37,20 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
     # a simple page that says hello
     @app.route("/hello")
     def hello():
         return "Hello, World!"
 
-    from . import db
+    # Registrar o comando de inicialização do banco de dados
+    app.cli.add_command(init_db_command)  # Adiciona o comando "init-db" à interface de linha de comando da aplicação
 
+    # Inicializar a extensão SQLAlchemy
     db.init_app(app)
 
     return app
